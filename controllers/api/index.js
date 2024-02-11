68c423f4e613691227a7511958c979408606e79d
treblot <treblotnad@gmail.com>
2024-02-10 18:31:48 -0600

    got log in working, need to finish routes and public js files. + Bulma styling

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-// const postRoutes = require("./postRoutes");
-// const commentRoutes = require("./commentRoutes");
+const postRoutes = require("./postRoutes");
+const commentRoutes = require("./commentRoutes");
 
-// router.use("/users", userRoutes);
-// router.use("/projects", postRoutes);
-// router.use("/comments", commentRoutes);
+router.use("/users", userRoutes);
+router.use("/projects", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,7 +2,7 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#post-name').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const description = document.querySelector('#post-desc').value.trim();
   
     if (name  && description) {
       const response = await fetch(`/api/post`, {
