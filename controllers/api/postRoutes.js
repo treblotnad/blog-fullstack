28b14a0c6c5b31f58859bcc789d7e21bafdd64e7
treblot <treblotnad@gmail.com>
2024-02-12 14:00:18 -0600

    delete route now working

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -14,4 +14,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
